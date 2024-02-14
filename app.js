fc34d53d623a55b75c410ad2cab776c2de5e6fb4
Elizabeth Bruda <elizabeth.bruda@gatech.edu>
2024-02-14 13:37:54 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,60 +1,30 @@
+const noble = require('noble');
+const bluetooth = require('bleat').webbluetooth;
+const fs = require('fs')
+const cheerio = require('cheerio')
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+var engines = require('consolidate');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("view engine", "ejs");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname)); //__dir and not _dir
+var port = 8000; // you can use any port
+app.listen(port);
+console.log('server on' + port);
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(bodyParser.json());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.post('/prediction_wrapper', async (req, res) => {
+  const $ = cheerio.load(fs.readFileSync('index.html'));
+
+
+  $("#prediction_val").text("no prediction")
+  res.send($.html())
+
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
